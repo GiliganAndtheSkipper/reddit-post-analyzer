74128c6a915cdea9bda9118387dbd0f656c7652a
GiliganAndtheSkipper <116029673+GiliganAndtheSkipper@users.noreply.github.com>
2024-08-17 15:26:32 -0400

    Delete subredditHealthApi.js

--- subredditHealthApi.js
+++
@@ -1,6 +0,0 @@
-// src/api/subredditHealthApi.js
-import axios from 'axios';
-
-export const fetchHealthData = async (subreddit) => {
-    return axios.get(`https://api.example.com/subreddit/${subreddit}/health`);
-};
