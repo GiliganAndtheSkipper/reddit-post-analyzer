b89da5a51ceca855134576224fffac70451eb4a4
Gil Garza <ggrzasd1982@gmail.com>
2024-08-18 21:52:52 -0400

    Add backend server logic and update server.js

--- reddit-post-analyzer/my-app/server.js
+++ reddit-post-analyzer/my-app/server.js
@@ -28,7 +28,7 @@ app.get('*', (req, res) => {
 });
 
 // Set the port
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
